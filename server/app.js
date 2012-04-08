582675e35fd53f3e32f9ae1dcd895ce69161ded5
xiaojue <designsor@gmail.com>
2012-04-08 23:22:13 +0400

    add clint rep

--- server/app.js
+++ server/app.js
@@ -6,11 +6,13 @@
 
 var http = require('http'),
 fs = require('fs'),
+path = require('path'),
 mxhrmanager = require('./mxhrmanager.js');
 
 var server = http.createServer(function(req, rep) {
 	var mxhrUrl = '/example.mxhr',
 	exampleUrl = '/example.html',
+    client = '/home/mxhrmanager/client/',
 	examplePath = '/home/mxhrmanager/public/example.html',
 	publicUrl = '/home/mxhrmanager/public/',
 	assest = function() {
@@ -52,7 +54,44 @@ var server = http.createServer(function(req, rep) {
 				console.log(err);
 			}
 		});
-	}
+	}else if(/mxhrmanager\/client/.test(req.url)){
+		fs.readFile(client+path.basename(req.url),function(err, data) {
+            var ext = path.extname(req.url).slice(1),
+                types = {
+                  "css": "text/css",
+                  "gif": "image/gif",
+                  "html": "text/html",
+                  "ico": "image/x-icon",
+                  "jpeg": "image/jpeg",
+                  "jpg": "image/jpeg",
+                  "js": "text/javascript",
+                  "json": "application/json",
+                  "pdf": "application/pdf",
+                  "png": "image/png",
+                  "svg": "image/svg+xml",
+                  "swf": "application/x-shockwave-flash",
+                  "tiff": "image/tiff",
+                  "txt": "text/plain",
+                  "wav": "audio/x-wav",
+                  "wma": "audio/x-ms-wma",
+                  "wmv": "video/x-ms-wmv",
+                  "xml": "text/xml"
+                },
+                type = types[ext];
+			    header = {
+				'Content-Type': type,
+				'Content-Length': data.length,
+				'server': 'node-server'
+			};
+			if (!err) {
+				rep.writeHead(200, header);
+				rep.write(data);
+				rep.end();
+			} else {
+				console.log(err);
+			}
+		});
+    }
 });
 
 server.listen(8000);
